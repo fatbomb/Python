93396a40cef515d001857fa685b2903a133a4106
fatbomb <joarderaniket@gmail.com>
2024-01-03 00:39:16 +0600

    committed now

---
+++ stringslicing.py
@@ -0,0 +1,16 @@
+#slicing = creating a substring by extracting elements from another string
+#          indexing[] or slice()
+#          [start:stop:step]
+# name="Altu faltu"
+# first_name=name[:4]
+# last_name= name[5:]
+# funky_name=name[::3]
+# reversed_name=name[::-1]
+# print(first_name)
+# print(last_name)
+# print(funky_name)
+# print(reversed_name)
+
+website="http://google.com"
+slice=slice(7,-4)
+print(website[slice])
